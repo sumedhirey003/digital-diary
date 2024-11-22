334b2b98e1b486181911c7069eddcf44f6e1d347
sumedhhirey003 <sumedhhirey003@gmail.com>
2024-11-22 19:08:47 +0530

    Initial commit

--- server/app.js
+++ server/app.js
@@ -25,7 +25,7 @@ console.log("Diary Routes Loaded:", diaryRoutes);
 
 console.log("Firebase DB Loaded:", db);
 
-const PORT = 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on port: ${PORT}`);
 });
