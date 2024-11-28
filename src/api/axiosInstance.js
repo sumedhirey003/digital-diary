c897de2663f9f71e9114f993811681cd46c28fdc
sumedhhirey003 <sumedhhirey003@gmail.com>
2024-11-28 20:13:20 +0530

    updated build and axios

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://digital-diary-gamma.vercel.app/api", // Update to match your backend's URL
+  baseURL: "https://digital-diary-two.vercel.app/api", // Update to match your backend's URL
   headers: {
     "Content-Type": "application/json",
   },
