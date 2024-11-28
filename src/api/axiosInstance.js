590364c7ebf715566e3a07bb8ee13f31b7801098
sumedhhirey003 <sumedhhirey003@gmail.com>
2024-11-29 03:15:45 +0530

    updated build axiosfile new build.

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://digital-diary-m26y.vercel.app/api",
   headers: {
     "Content-Type": "application/json",
   },
